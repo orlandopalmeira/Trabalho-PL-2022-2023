495826733f97c64d28c83ec652d178597ce31861
Katilho <pemicama12@gmail.com>
2023-04-17 22:28:40 +0100

    probable final fix to multistring many quotes

--- src/program.py
+++ src/program.py
@@ -4,7 +4,8 @@ from grammar import parser
 
 ### Main
 # python3 program.py (<ficheiro_input>) (<ficheiro_output>)
-in_file = "/home/pedro/PL/Trabalho-PL-2022-2023/src/examples/strings.toml"
+# in_file = "/home/pedro/PL/Trabalho-PL-2022-2023/src/examples/strings.toml"
+in_file = "/home/pedro/PL/Trabalho-PL-2022-2023/src/examples/zcurrent.toml"
 out_file = "result.json"
 if len(sys.argv) > 2:
     out_file = sys.argv[2] 
